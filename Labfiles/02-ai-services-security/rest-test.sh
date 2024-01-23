5c3b59052ddcf2925e6382ebc598c45f628ab8a1
Wahid Alimi <wahid.alimi@gmail.com>
2024-01-23 09:46:42 -0500

    added Azure resource template zip file for Azure AI service and keyvault.

--- Labfiles/02-ai-services-security/rest-test.sh
+++ Labfiles/02-ai-services-security/rest-test.sh
@@ -1,1 +1,1 @@
-curl -X POST "<your-endpoint>/text/analytics/v3.1/languages?'" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <your-key>" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
+curl -X POST "https://secureaiservices.cognitiveservices.azure.com/language/:analyze-text?api-version=2023-04-01" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: 5e12de82308a438bb124c1662a3d2966" --data-ascii "{'analysisInput':{'documents':[{'id':1,'text':'hello'}]}, 'kind': 'LanguageDetection'}"
