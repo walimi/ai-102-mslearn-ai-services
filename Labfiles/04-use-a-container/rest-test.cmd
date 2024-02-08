d786aed24378246b31c20b6d607b05ec31b2e7d8
Wahid Alimi <wahid.alimi@gmail.com>
2024-02-08 12:01:13 -0500

    I don't know what's in this commit.

--- Labfiles/04-use-a-container/rest-test.cmd
+++ Labfiles/04-use-a-container/rest-test.cmd
@@ -1,1 +1,1 @@
-curl -X POST "http://<your_ACI_IP_address_or_FQDN>:5000/text/analytics/v3.0/languages" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
+curl -X POST "http://waai102usecontainers.gkgzgxdyeefkedcb.eastus.azurecontainer.io:5000/text/analytics/v3.0/languages" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
